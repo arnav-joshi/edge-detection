9e1f6d4eb60b1b430655420a68b934838c735b0a
Arnav Joshi <74690929+arnav-joshi@users.noreply.github.com>
2021-01-30 14:08:01 -0600

    Better framerate, no pyplot
    xzsdhjyugvnmjlkihymgvnb ik8]hjmvnjuhynmb ,mojihgvbn ki

--- edgecam.py
+++ edgecam.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 def on_trackbar(val):
@@ -9,11 +8,11 @@ def on_trackbar(val):
   mint = cv2.getTrackbarPos("min", "levels")
   maxt = cv2.getTrackbarPos("max", "levels")
   edges = cv2.Canny(img,mint,maxt)
-  cv2.imshow(levels,img)
-  cv2.imshow(levels,img) 
+  cv2.imshow("levels",img)
+  cv2.imshow("levels",edges) 
   
 
-  cv2.namedWindow("levels")
+cv2.namedWindow("levels")
 cv2.createTrackbar("min", "levels", 0, 500, on_trackbar) 
 cv2.createTrackbar("max", "levels", 0, 500, on_trackbar) 
 
