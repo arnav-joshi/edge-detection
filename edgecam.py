57b867ed98eea4fe7d1ad376cdcc163e44e1ee01
Arnav Joshi <74690929+arnav-joshi@users.noreply.github.com>
2021-01-30 13:57:31 -0600

    new test branch
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, no pyplot

--- edgecam.py
+++ edgecam.py
@@ -9,13 +9,11 @@ def on_trackbar(val):
   mint = cv2.getTrackbarPos("min", "levels")
   maxt = cv2.getTrackbarPos("max", "levels")
   edges = cv2.Canny(img,mint,maxt)
-  plt.subplot(121),plt.imshow(img,cmap = 'gray')
-  plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-  plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-  plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-  plt.show()
+  cv2.imshow(levels,img)
+  cv2.imshow(levels,img) 
   
-cv2.namedWindow("levels")
+
+  cv2.namedWindow("levels")
 cv2.createTrackbar("min", "levels", 0, 500, on_trackbar) 
 cv2.createTrackbar("max", "levels", 0, 500, on_trackbar) 
 
